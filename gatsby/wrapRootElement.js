9f6895e0376870cd53a519d709e5ee9df590fe46
Kaio Barbosa Gama <kaiogama18@hotmail.com>
2020-11-10 13:16:45 -0400

    Helmet

--- gatsby/wrapRootElement.js
+++ gatsby/wrapRootElement.js
@@ -3,12 +3,18 @@ import light from '../src/styles/themes/light'
 import { GlobalStyle } from '../src/styles/styles';
 import { Footer, Header } from '../src/components/index';
 import { Wrapper } from '../src/styles/styles';
+import {Helmet} from 'react-helmet';
+
 
 const { ThemeProvider } = require('styled-components');
 
 export function wrapRootElement({ element }) {
     return <ThemeProvider theme={light}  >
         <GlobalStyle />
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Buritech | Soluções em Machine Learning</title>
+        </Helmet>
         <Header />
         <Wrapper>
             {element}
