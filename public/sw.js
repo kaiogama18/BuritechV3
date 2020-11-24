0552ba2ac8ea52351bd607ad819f5e0c0cc64fc8
Kaio Barbosa Gama <kaiogama18@hotmail.com>
2020-11-23 22:27:55 -0400

    ajuste

--- public/sw.js
+++ public/sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5a1b8a0bae699ba4f034.js"
+    "url": "webpack-runtime-d563e20204138136008a.js"
   },
   {
-    "url": "styles.89e4848987029cab0ed3.css"
+    "url": "styles.55acf44fb1f30b557d30.css"
   },
   {
     "url": "styles-755093da0c07f4b49226.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-8e528b732ab2eaadb7b7.js"
   },
   {
-    "url": "app-8c2bb16162b8939beb20.js"
+    "url": "app-b11ed7bea78aa524ca6d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4cb4396a82a79f14b6d0e06aeca8162a"
+    "revision": "871b32440e00d2a2b25f24b0774fcf0e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -54,10 +54,10 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "be1ab840572f568d1ac74816b5b0188c"
+    "revision": "ed9610b2917150dd161ceb0bc4bf55e8"
   },
   {
-    "url": "polyfill-0ea1cb888e32e84b5d2e.js"
+    "url": "polyfill-f16735f449f606c8d4cd.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -153,7 +153,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8c2bb16162b8939beb20.js`))) {
+  if (!resources || !(await caches.match(`/app-b11ed7bea78aa524ca6d.js`))) {
     return await fetch(event.request)
   }
 
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -14,26 +14,18 @@ export default () => {
         negócio da sua empresa, como sites para venda de produtos/serviços,
         extração de dados para criação de relatórios gerenciais, entre outros
         serviços.
-
         
 
         Aplicativos para Android e iOS também podem ser desenvolvidos para
         auxiliar nos negócios dos nossos clientes.
-
 
         Em geral, já há um sistema de retaguarda e API de acesso às bases de
         dados. Nesse caso, nosso time de desenvolvimento trabalha em
         parceria com o time técnico do cliente na construção do aplicativo.
       </Content>
 
-      <Item flex>
-        <Title normal>Metodologia para sites e aplicativos:  </Title>
-        
       </Item>
 
-      </Item>
-
-     
     </Container>
   )
 }
