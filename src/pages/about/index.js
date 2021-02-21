931eee38e1aa1af6a6d2912a4ea146c84fde0429
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-20 22:53:26 -0400

    [text] --> resume 2021
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,80 +1,81 @@
-import React from 'react';
-import Img from "gatsby-image";
-import { graphql, useStaticQuery } from 'gatsby';
-import { Container, Content, Item, Title, Button } from "../../components"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import React from "react"
+import { Button, Container, Content, Item, Title } from "../../components"
 
 export default () => {
-
-    const data = useStaticQuery(graphql`
-        query {
-            img1: file(relativePath: { eq: "about/img1.webp" }) {
-            base
-            childImageSharp {
-                fluid(maxWidth: 1000, quality: 100) {
-                    ...GatsbyImageSharpFluid_withWebp
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query {
+      img1: file(relativePath: { eq: "about/img1.webp" }) {
+        base
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+           }
         }
-            img2: file(
-                relativePath: { eq: "about/img2.webp" }
-            ) {
-                base
-                childImageSharp {
-                    fluid(maxWidth: 2000, quality: 100) {
-                        ...GatsbyImageSharpFluid_withWebp
-                    }
-                }
-            }
+      }
+      img2: file(relativePath: { eq: "about/img2.webp" }) {
+        base
+        childImageSharp {
+          fluid(maxWidth: 2000, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
         }
-    `)
+      }
+    }
+  `)
 
-    return (
-        <Container vertical>
-            <Container>
+  return (
+    <Container vertical>
+      <Container>
+        <Item flex>
+          <Title> A BURITECH </Title>
+          <Content>
+            Somos uma empresa regional especializada em{" "}
+            <b> desenvolvimento de software</b> e na realização de treinamentos
+            técnicos em linguagens de programação, engenharia de softwares e
+            áreas relacionadas.
+          </Content>
+          <Content>
+            A Buritech é uma empresa de base tecnológica, sediada na cidade de
+            Manaus, que oferece a seus clientes e parceiros soluções em projetos
+            de software, consultoria e treinamentos voltados à
+            <b> Ciência de Dados</b>, desenvolvimento e{" "}
+            <b>aplicações Web e Mobile.</b>
+          </Content>
+        </Item>
 
-                <Item flex>
-                    <Title>  A BURITECH </Title>
-                    <Content>
-                        Somos uma empresa regional especializada em <b> desenvolvimento de software</b> e na realização de treinamentos técnicos em linguagens de programação, engenharia de softwares e áreas relacionadas.
-                    </Content>
-                    <Content>
-                        A Buritech é uma empresa de base tecnológica, sediada na cidade
-                        de Manaus, que oferece a seus clientes
-                        e parceiros soluções em projetos de software, consultoria e treinamentos voltados à
-                        <b> Ciência de Dados</b>, desenvolvimento e <b>aplicações Web e Mobile.</b>
-                    </Content>
-                </Item>
+        <Item flex>
+          <Img fluid={data.img1.childImageSharp.fluid} />
+        </Item>
+      </Container>
 
-                <Item flex>
-                    <Img fluid={data.img1.childImageSharp.fluid} />
-                </Item>
-            </Container>
+      <Container>
+        <Item flex>
+          <Img fluid={data.img2.childImageSharp.fluid} />
+        </Item>
+        <Item flex>
+          <Title> POR QUE MACHINE LEARNING? </Title>
+          <Content>
+            Machine Learning é a parte da inteligência artificial que permite
+            que sistemas aprendam automaticamente a partir da experiência.
+          </Content>
+          <Content>
+            Data Science é a ciência que estuda as informações, seu processo de
+            captura, transformação, geração e, posteriormente, análise de dados.
+          </Content>
 
-            <Container>
-                <Item flex>
-                    <Img fluid={data.img2.childImageSharp.fluid} />
-                </Item>
-                <Item flex>
-                    <Title>  POR QUE MACHINE LEARNING? </Title>
-                    <Content>
-                        Machine Learning é a parte da inteligência artificial
-                        que permite que sistemas aprendam automaticamente a partir da experiência.
-                    </Content>
-                    <Content>
-                        Data Science é a ciência que estuda as informações, seu processo de captura,
-                        transformação, geração e, posteriormente, análise de dados.
-                    </Content>
-
-                    <Content>
-                        Nossos projetos de web, como o desenvolvimento de
-                        sites, aplicativos e chatbots, são esquematizados a partir da inteligência do Machine Learning. <br />
-                        <Button text="veja nossos projetos" link="https://github.com/buritech" />
-                    </Content>
-
-                </Item>
-            </Container>
-        </Container>
-    )
+          <Content>
+            Nossos projetos de web, como o desenvolvimento de sites, aplicativos
+            e chatbots, são esquematizados a partir da inteligência do Machine
+            Learning. <br />
+            <Button
+              text="veja nossos projetos"
+              link="https://github.com/buritech"
+            />
+          </Content>
+        </Item>
+      </Container>
+    </Container>
+  )
 }
-
-
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,29 +1,27 @@
 import React from "react"
-import { Form } from './styles'
-import { Container, Item, Title, Button } from "../../components"
+import { Button, Container, Item, Title } from "../../components"
+import { Form } from "./styles"
 
 export default () => {
   return (
     <Form>
       <Container vertical>
-      <Item flex align={"center"}>
-        <Title>  Nos envie um email: </Title>
-      </Item>
-
+        <Item flex align={"center"}>
+          <Title> Nos envie um email: </Title>
+        </Item>
       </Container>
-      <Container backgroundColor={'#edf3f9'} >
+      <Container backgroundColor={"#edf3f9"}>
         <Item flex align={"center"}>
           <form action="/" id="usrform">
+            <label htmlFor="usrname">Seu nome:</label>
+            <input type="text" name="usrname" value="" />
 
-            <label  htmlFor="usrname">Seu nome:</label>
-            <input type="text" name="usrname" value=""/>
+            <label htmlFor="lname">Mensagem:</label>
+            <textarea rows="4" cols="50" name="comment" form="usrform" />
 
-            <label  htmlFor="lname">Mensagem:</label>
-            <textarea rows="4" cols="50" name="comment" form="usrform"/>
-           
-            <label  htmlFor="usremail">Seu email:</label>
-            <input type="email" name="usremail" value=""/>
-            
+            <label htmlFor="usremail">Seu email:</label>
+            <input type="email" name="usremail" value="" />
+
             <Button type="submit" text="Enviar" link="/" />
           </form>
         </Item>
