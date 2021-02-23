40ecfa35407761f45608d9e4267880f8f67800cf
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-23 01:49:50 -0400

    fixed error ssr style components
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -27,29 +27,29 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1e45e15d9a67238e958f.js"
-  },
-  {
-    "url": "styles.55acf44fb1f30b557d30.css"
-  },
-  {
-    "url": "styles-755093da0c07f4b49226.js"
+    "url": "webpack-runtime-e655fef18fd015983598.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
   },
   {
-    "url": "app-65e803a37598c9c00bf0.js"
+    "url": "styles.ccf3c7b2d1ea0f474ee8.css"
+  },
+  {
+    "url": "styles-755093da0c07f4b49226.js"
+  },
+  {
+    "url": "app-e5c6041d2c3e30d37721.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3f7a4b55ab9334df56487298291df829"
+    "revision": "7173232ab2031d0ece921afe909d5a5f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
   },
   {
-    "url": "polyfill-f16735f449f606c8d4cd.js"
+    "url": "polyfill-737f5fea40ba29bc8b2c.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -145,7 +145,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-65e803a37598c9c00bf0.js`))) {
+  if (!resources || !(await caches.match(`/app-e5c6041d2c3e30d37721.js`))) {
     return await fetch(event.request)
   }
 
